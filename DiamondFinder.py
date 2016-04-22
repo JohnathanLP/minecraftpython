0e080bc284eb002b384ca9864be679e9da451554
Johnathan <JohnathanLP@users.noreply.github.com>
2016-04-21 20:51:00 -0600

    commenting
    added some comments and a description

--- DiamondFinder.py
+++ DiamondFinder.py
@@ -1,3 +1,8 @@
+#DiamondFinder - Written by Johnathan Powell for Cache Makers
+#Program activate when a button is pressed on pin 14 (using BCM numbering)
+#Searches a 3x3 collumn below the player to the bottom of the world for diamond ore, if 
+#ore is found, light will illuminate and the number of blocks found will be output in chat.
+
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 import time
@@ -18,17 +23,20 @@ try:
             mc.postToChat("Now finding diamonds...")
             found = 0
             x,y,z = mc.player.getPos()
-            test = y
-            while test > -63:
-                bi = mc.getBlock(x,test,z)
+            ytest = y
+            #search down to the bottom of the world
+            while ytest > -63:
+                bi = mc.getBlock(x,ytest,z)
+                #if block is a diamond
                 if bi == 56:
                     mc.postToChat("Diamonds found!")
                     GPIO.output(ledPin, True)
-                    found = (found + 1)
-                test = (test - 1)
-
+                    found = (found + 1) 
+                ytest = (ytest - 1)
+            #if no diamonds are found
             if found == 0:
                 mc.postToChat("No diamonds found!")
+            #if diamonds are found
             else:
                 string = "Number of diamonds found: "
                 string += `found`
